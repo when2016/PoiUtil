dadc14e0228d1165609fb7c52be24773ae54948e
wanghongen <whenmm@gmail.com>
2018-03-07 10:21:33 +0800

    'testA'

--- src/main/java/com/wanghongen/utils/TestA.java
+++ src/main/java/com/wanghongen/utils/TestA.java
@@ -7,6 +7,7 @@ import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.apache.poi.ss.usermodel.Cell;
 
 /**
  * 一个例子说明POI解析EXCEL的大致原理：（取自网上已有–一个粗糙例子）取自此文 某管理员要查某层楼有多少人叫什么名字？ 1)首先要明确大楼在那里(找到对应的文件)
@@ -16,7 +17,7 @@ public class TestA {
 
   public static void main(String args[]) throws Exception {
     //找到大楼的位置
-    FileInputStream input = new FileInputStream("d:\\dir.xls");
+    FileInputStream input = new FileInputStream("E://工单信息表bean.xls");
     //告诉管理员
     POIFSFileSystem f = new POIFSFileSystem(input);
     //走到大楼楼下
@@ -35,10 +36,36 @@ public class TestA {
         HSSFCell cell = (HSSFCell) cells.next();
         //是先生还是小姐(对应的数据类型)
         int cellType = cell.getCellType();
-        //System.out.print(getValue(cell,cellType));
+        System.out.print(getValue(cell,cellType));
+
+        // 是先生还是小姐(对应的数据类型)
+//        System.out.print(cell.getStringCellValue() + "====|===");
+
       }
       System.out.println("");
     }
+  }
+
+  /**
+   * 值对象封装
+   */
+  public static Object getValue(Cell cell, int cellType) {
+    if (cellType == Cell.CELL_TYPE_NUMERIC) {
+      return cell.getNumericCellValue() + "       |   ";
+    } else if (cellType == Cell.CELL_TYPE_STRING) {
+      return cell.getRichStringCellValue() + "        |   ";
+    } else if (cellType == Cell.CELL_TYPE_BOOLEAN) {
+      return cell.getBooleanCellValue() + "       |   ";
+    } else if (cellType == Cell.CELL_TYPE_FORMULA) {
+      return cell.getCellFormula() + "        |   ";
+    } else if (cellType == Cell.CELL_TYPE_BLANK) {
+      return "" + "       |   ";
+    } else if (cellType == Cell.CELL_TYPE_ERROR) {
+      return "" + "       |   ";
+    } else {
+      return "" + "       |   ";
+    }
+
   }
 
 }
